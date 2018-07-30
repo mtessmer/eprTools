7549ecf698f23b82918bec89757873300671c791
Maxx Tessmer <mhtessmer@gmail.com>
2018-07-30 17:50:34 -0500

    Added setup.py for easy installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name = 'eprTools',
+      version = '0.1',
+      description = 'python tools for EPR',
+      author = 'Maxx Tessmer',
+      author_email = 'mhtessmer@gmail.com',
+      requires = ['numpy', 'scipy', 'sklearn', 'matplotlib', 'numba'],
+      packages=['eprTools'])
+      
