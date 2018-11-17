b213928f325f599cdf9aeb3ba4d02ed277b029f3
Maxx Tessmer <mhtessmer@gmail.com>
2018-11-17 13:48:25 -0600

    updated to match changed class name

--- examples/deer_example.py
+++ examples/deer_example.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-from eprTools import DEER_spec
+from eprTools import DEERSpec
 
-data = DEER_spec.from_file('Example_DEER.DTA')
+data = DEERSpec.from_file('Example_DEER.DTA')
 data.set_kernel_len(500)
 
 data.get_fit()
+
 
 fig, (ax1, ax2) = plt.subplots(1,2, figsize = [20, 10.5])
 ax1.plot(data.time, data.dipolar_evolution)
