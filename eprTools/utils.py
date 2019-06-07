93858aed8e2926fb00207759b8d703fab29945b1
maxx <mhtessmer@gmail.com>
2019-06-07 14:05:03 -0700

    fixed divide by 0 error

--- eprTools/utils.py
+++ eprTools/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.special import fresnel
-from sklearn.linear_model.base import LinearModel
 
 
 def fredholm_kernel(rmin=15, rmax=80, time=3500, size=200):
@@ -9,7 +8,7 @@ def fredholm_kernel(rmin=15, rmax=80, time=3500, size=200):
         time = time * 1000
 
     r = np.linspace(rmin, rmax, size)
-    time = np.linspace(0, time, size)
+    time = np.linspace(0.01, time, size)
 
     omega_dd = (2 * np.pi * 52.0410) / (r ** 3)
     trigterm = np.outer(time, omega_dd)
@@ -25,10 +24,9 @@ def fredholm_kernel(rmin=15, rmax=80, time=3500, size=200):
     return K
 
 def background(a, d = 3, time = 3500, size = 200):
-
     if time < 10:
         time = time * 1000
 
     time = np.linspace(0, time, size)
 
-    return a * np.exp(-k * (time ** (d / 3)) + j)
+    return a * np.exp(-k * (time ** (d / 3)))
