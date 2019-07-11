c4ffd2e653dd23b4594ca866648c97bc61f39ac0
maxx <mhtessmer@gmail.com>
2019-07-11 13:38:33 -0700

    updated and improved samples

--- examples/cw_example.py
+++ examples/cw_example.py
@@ -1,7 +1,11 @@
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 from eprTools import CWSpec
 
-mySpc = CWSpec.from_file('Example_Apo.DTA', preprocess = True)
-plt.plot(mySpc.field, mySpc.spec)
+mySpc1 = CWSpec.from_file('Example_Apo.DTA', preprocess = True)
+mySpc2 = CWSpec.from_file('Example_Holo.DTA', preprocess = True)
+
+plt.plot(mySpc1.field, mySpc1.spec)
+plt.plot(mySpc2.field, mySpc2.spec)
 plt.show()
 
--- examples/deer_example.py
+++ examples/deer_example.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 import matplotlib.pyplot as plt
 from eprTools import DEERSpec
 
@@ -10,12 +10,20 @@ spc.set_kernel_len(250)                                                 # todo a
 
 spc.get_fit()
 
-
-
 fig, (ax1, ax2) = plt.subplots(1,2, figsize = [20, 10.5])
 ax1.plot(spc.time, spc.dipolar_evolution)
 ax1.plot(spc.fit_time, spc.fit)
 ax2.plot(spc.r, spc.P)
+plt.show()
+
+
+
+#Get L-curve
+rho, eta, alpha_idx = spc.get_L_curve()
+
+fig2, ax = plt.subplots()
+ax.scatter(rho, eta)
+ax.scatter(rho[alpha_idx], eta[alpha_idx], c = 'r', facecolor=None)
 plt.show()
 
 print(spc.alpha)
