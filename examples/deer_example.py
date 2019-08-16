663f76922aafc73631eb72d8e1a67304b66f08f0
Maxx Tessmer <mhtessmer@gmail.com>
2019-08-16 01:21:39 -0700

    cleanup

--- examples/deer_example.py
+++ examples/deer_example.py
@@ -2,33 +2,36 @@
 import matplotlib.pyplot as plt
 from eprTools import DEERSpec
 from eprTools import do_it_for_me
-from time import  time
+from time import time
 
-#do_it_for_me('Example_DEER.DTA', method = None)
-#do_it_for_me('Example_DEER.DTA', method = None)
-spc = DEERSpec.from_file('Example_DEER.DTA')
-print("background: ", spc.background_param)
+fit_methods = ['nnls', 'nnls_gd', 'cvx']
+for fit_method in fit_methods:
 
-spc.set_kernel_r(rmin=15, rmax=80)
-spc.set_kernel_len(250)                                                 # todo add examples for all features
+    do_it_for_me('Example_DEER.DTA', fit_method=fit_method)
 
-spc.get_fit()
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=[20, 10.5])
-ax1.plot(spc.time, spc.dipolar_evolution)
-ax1.plot(spc.fit_time, spc.fit)
-ax2.plot(spc.r, spc.P)
-plt.show()
+#spc = DEERSpec.from_file('Example_DEER.DTA')
+
+#spc.set_kernel_r(rmin=15, rmax=80)
+#spc.set_kernel_len(250)              # todo add examples for all features
+
+#spc.get_fit()
+
+#fig, (ax1, ax2) = plt.subplots(1, 2, figsize=[20, 10.5])
+#ax1.plot(spc.time, spc.dipolar_evolution)
+#ax1.plot(spc.fit_time, spc.fit)
+#ax2.plot(spc.r, spc.P)
+#plt.show()
 
 
 
 #Get L-curve
-rho, eta, alpha_idx = spc.get_L_curve()
+#rho, eta, alpha_idx = spc.get_L_curve()
 
-fig2, ax = plt.subplots()
-ax.scatter(rho, eta)
-ax.scatter(rho[alpha_idx], eta[alpha_idx], c='r', facecolor=None)
-plt.show()
+#fig2, ax = plt.subplots()
+#ax.scatter(rho, eta)
+#ax.scatter(rho[alpha_idx], eta[alpha_idx], c='r', facecolor=None)
+#plt.show()
 
-print(spc.alpha)
+#print(spc.alpha)
 
