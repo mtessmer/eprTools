102010643b4b580d049d8ae5301dedb328eab5b8
Maxx Tessmer <mhtessmer@gmail.com>
2020-07-21 12:22:57 -0700

    Made kernel generation easier with broader input options. Removed generate_kernel_nm since it is no longer needed. Added functionality to go negative in time domain when generating kernel.

--- eprTools/utils.py
+++ eprTools/utils.py
@@ -1,18 +1,19 @@
+import numbers
+from collections.abc import Sized
 import numpy as np
 from numba import njit
 from scipy.special import fresnel
 from scipy.optimize import curve_fit
 
 
-def generate_kernel(rmin=15, rmax=80, time=3500, size=200):
+def generate_kernel(r=(15, 80), time=3500, **kwargs):
     """
     Generate DEER kernel using  angstroms and nanoseconds
 
-    :param rmin: float
-        minimum distance covered by kernel (Å)
-
-    :param rmax: float
-        maximum distance covered by kernel (Å)
+    :param r: float, tuple, sized container
+        Distance domain of the kernel.
+        if float, distance domain will go from 1-float
+        if tuple of length 2, the distance domain represents the min and max
 
     :param time: float
         maximum time covered by kernel (ns)
@@ -24,58 +25,49 @@ def generate_kernel(rmin=15, rmax=80, time=3500, size=200):
         DEER Kernel matrix
     """
 
-    # Ensure units to ns
-    if time < 10:
-        time = time * 1000
 
-    r = np.linspace(rmin, rmax, size)
-    time = np.linspace(0, time, size)
+    if 'size' in kwargs:
+        size = kwargs['size']
+    else:
+        size = 256
+
+    # Interpret distance domain
+    if isinstance(r, numbers.Number):
+        r = np.linspace(1, r, size)
+    elif isinstance(r, Sized):
+        if len(r) == 2:
+            r = np.linspace(r[0], r[1], size)
+
+        else:
+            r = np.asarray(r)
+
+    else:
+        raise ValueError("Unrecognized value for distance domain.")
+
+    # Interpret time domain
+    if isinstance(time, numbers.Number):
+        time = np.linspace(1, time, size)
+    elif isinstance(r, Sized):
+        if len(time) == 2:
+            r = np.linspace(time[0], time[1], size)
+
+        else:
+            time = np.asarray(time)
+
+    else:
+        raise ValueError("Unrecognized value for time domain.")
 
     omega_dd = (2 * np.pi * 52.0410) / (r ** 3)
-    trigterm = np.outer(time, omega_dd)
+    trigterm = np.outer(np.abs(time), omega_dd)
     z = np.sqrt((6 * trigterm / np.pi))
     S_z, C_z = fresnel(z) / z
     K = C_z * np.cos(trigterm) + S_z * np.sin(trigterm)
 
     # Correct for error introduced by divide by zero error
-    K[0] = 1
+    K[time == 0] = 1
 
     return K
 
-
-def generate_kernel_nm(rmin=1.5, rmax=8.0, time=3.5, size=200):
-    """
-    Generate DEER kernel using  nanometers and microsecond
-
-    :param rmin: float
-        minimum distance covered by kernel (nm)
-
-    :param rmax: float
-        maximum distance covered by kernel (nm)
-
-    :param time: float
-        maximum time covered by kernel (μs)
-
-    :param size: int
-        axis size of kernel matrix
-
-    :return: numpy ndarray
-        DEER Kernel matrix
-
-    """
-    r = np.linspace(rmin, rmax, size)
-    time = np.linspace(0, time, size)
-
-    omega_dd = (2 * np.pi * 52.0410) / (r ** 3)
-    trigterm = np.outer(time, omega_dd)
-    z = np.sqrt((6 * trigterm / np.pi))
-    S_z, C_z = fresnel(z) / z
-    K = C_z * np.cos(trigterm) + S_z * np.sin(trigterm)
-
-    # Correct for error introduced by divide by zero error
-    K[0] = 1
-
-    return K
 
 def homogeneous_3d(t, k, a, d=3):
     """
