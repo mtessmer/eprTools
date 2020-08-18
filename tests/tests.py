bd2282c010a33689946ce28e047f743af94ed1ca
Maxx Tessmer <mhtessmer@gmail.com>
2020-08-18 13:57:13 -0700

    Fixed generate kernel tests

--- tests/tests.py
+++ tests/tests.py
@@ -10,15 +10,15 @@ from time import time
 r = np.linspace(15, 100, 2 ** 10)
 t = np.linspace(-1000, 4500, 2 ** 10)
 
-ans_list = []
-with np.load('test_data/gen_kern.npz') as f:
-    for K in f.keys():
-        ans_list.append(f[K])
-
 kws = [{}, {'r': 80, 'time': 2500},
        {'r': r, 'time': 5000},
        {'size': 100},
        {'time': t, 'size': 100}]
+
+ans_list = []
+with np.load('test_data/gen_kern.npz') as f:
+    for K in f.keys():
+        ans_list.append(f[K])
 
 test_data = zip(kws, ans_list)
 
@@ -178,8 +178,8 @@ class TestUtils:
         rnm = np.linspace(1.5, 10, 2**10)
         tnm = np.linspace(-1, 4.5, 2**10)
 
-        Knm = utils.generate_kernel(rnm, tnm)
-        K = utils.generate_kernel(r, t)
+        Knm, _, _ = utils.generate_kernel(rnm, tnm)
+        K, _, _ = utils.generate_kernel(r, t)
 
         np.testing.assert_almost_equal(K, Knm)
 
