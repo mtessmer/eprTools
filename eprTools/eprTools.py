c8a33c7d2de496ea1e17f11b928d4eb4e680d8f1
Maxx Tessmer <mhtessmer@gmail.com>
2021-01-28 09:15:10 -0800

    Improved some formatting

--- eprTools/eprTools.py
+++ eprTools/eprTools.py
@@ -78,14 +78,14 @@ def eprload(file, preprocess = False, k=0):
     
     return DTA_data
     
-#Perform offset baseline correction using ALL data points
+# Perform offset baseline correction using ALL data points
 def basecorr(spectra, k=0, ends = 25):
     xdata = spectra[0]
     spectra = spectra[1]
-    #Create an X axis vector for the spectra
+    # Create an X axis vector for the spectra
     x = np.arange(len(spectra))
     
-    #Conditional for higher order fit
+    # Conditional for higher order fit
     if k > 0:
         subX = np.concatenate([x[:ends], x[-ends:]])
         subY = spectra[subX]
@@ -93,44 +93,46 @@ def basecorr(spectra, k=0, ends = 25):
         bground = np.polyval(fit1, x)
         spectra = spectra - bground
     
-    #Perform polynomial fit (Should be the average off all points)
+    # Perform polynomial fit (Should be the average off all points)
     pfit = np.polyfit(x, spectra, 0)
     yfit = np.polyval(pfit,x)
     
-    #Subtract fit from data 
+    # Subtract fit from data
     specSubt = np.subtract(spectra, yfit)
     
-    #return data
+    # return data
     out_data = np.array([xdata, specSubt])
     return out_data
 
-#Center Spectrra arround midpoint
+
+# Center Spectra around midpoint
 def center(spectra, midmax=False):
     xdata = spectra[0]
     spectra = spectra[1]
     
-    #Find the min and max of the spectra
+    # Find the min and max of the spectra
     Xmax = np.argmax(spectra)
     Xmin = np.argmin(spectra)
     
-    #Take a subset of min and max to find midpoint
+    # Take a subset of min and max to find midpoint
     myMidSub = spectra[Xmax:Xmin]
     subMidpoint = (np.abs(myMidSub)).argmin()
     midPoint = Xmax + subMidpoint
     LefRig = 475
     
-    #Logic re midmoint is peak or 0 cross 
+    # Logic re midmoint is peak or 0 cross
     if midmax:
         midPoint = Xmax
         LefRig = 450
     
-    #Create 1001 length vector centered at midpoint
+    # Create 1001 length vector centered at midpoint
     newMin = midPoint - LefRig
     newMax = midPoint + LefRig
     centerSpec = spectra[newMin:newMax]
-    
-    #Return the centered spectra
+
+    # Return the centered spectra
     return centerSpec
+
 
 def normalize(spc):
     xdata = spc[0]
@@ -138,6 +140,7 @@ def normalize(spc):
     ydata = spc / np.trapz(cumtrapz(spc, xdata, initial=0), xdata)
     out = np.array([xdata, ydata])
     return out
+
 
 def abs_half_moment(spc):
     xdata = np.arange(0,100,(100/len(spc[1]))) #spc[0]
